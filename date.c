124f1a9b21f6d7d688f5c45d395e248c3cb96d69
John Aldrin B. Bautista <aldrinbautista0724@yahoo.com>
2016-12-20 09:19:36 +0800

    Create date.c

---
+++ date.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+int main(){
+	int month;
+	int day;
+	printf("enter the numbet of month(1-12):");
+	scanf("%d", & month);
+	printf(" enter the day number(1-7):");
+	scanf("%d", & day);
+	if(month==1);{
+	printf("jan");
+	}
+
+	
+	if (month==2){
+	printf("feb");
+}
+	
+	
+	if(month==3){
+	printf("mar");
+	}
+	
+	
+	
+	if(month==4){
+	printf("apr");
+}
+	
+	
+	if(month==5){
+	printf("may");
+}
+	
+	
+		if(month==6){
+		printf("jun");
+	}
+	
+	if(month==7){
+	printf("jul");
+}
+	
+		if(month==8){
+		printf("aug");
+		}
+
+		if(month==9){
+		printf("sep");
+	}
+	
+	
+		if(month==10){
+		printf("oct");
+	}
+		
+	
+		if(month==11){
+		printf("nov");
+	}
+	
+	
+		if(month==12){
+		printf("dec");
+	}
+	
+	if(day==1){
+	printf("sun");
+}
+	if(day==2){
+	printf("mon");
+}
+	if(day==3){
+	printf("teus");
+}
+	if(day==4){
+		printf("wed");
+	}
+	if(day==5){
+		printf("thurs");
+}
+	if(day==6){
+		printf("fri");
+	}
+	if(day==7){
+		printf("sat");
+	}
+}
